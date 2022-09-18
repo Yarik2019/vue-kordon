9faa0741c980b7eeb32a8dca4566fd74d2f2b861
Yarik2019 <yroslavterno@gmail.com>
2022-09-18 13:26:26 +0300

    build

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('./assets/scss/_custom.scss');
+@import url('./assets/scss/_custom.css');
 // #app {
 //   font-family: Avenir, Helvetica, Arial, sans-serif;
 //   -webkit-font-smoothing: antialiased;
